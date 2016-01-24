682e8ee642fb48d72379c6c9735cf2a52d425f64
Stuart <Rushmead@users.noreply.github.com>
2016-01-24 19:28:22 +0000

    Fix wierd issue

--- js/main.js
+++ js/main.js
@@ -9,38 +9,39 @@ $(document).ready(function(event){
    });
 });
 function getData(){
-    $.get('https://api.twitch.tv/kraken/channels/' + $('#inputText').val(), "", function(data){
-         
-         var avatar = data['logo'];
-         var username = data['display_name'];
-         var followers = data['followers'];
-         var totalViews = data['views'];
-         var sub = data['partner'];
-         var mature = data['mature'];
-         var online = data['status'];
-         if(data['game'] != null){
-            var lastPlayed = data['game'];
+    $.get('https://beam.pro/api/v1/channels/' + $('#inputText').val(), "", function(data){
+         var avatar = "https://beam.pro/api/v1/users/" + data['user']['id'] + "/avatar";
+         var username = data['user']['username'];
+         var followers = data['numFollowers'];
+         var experience = data['user']['experience'];
+         var sparks = data['user']['sparks'];
+         var channelID = data['user']['id'];
+         var totalViews = data['viewersTotal'];
+         var online = data['online'];
+         if(data['type'] != null){
+            var lastPlayed = data['type']['name'];
          }else{
             var lastPlayed = "None";
          }
-         var joined = data['created_at'];
+         var joined = data['createdAt'];
          var html = '<center><img src="' + avatar + '"width="100px" height="100px" style="border:3px solid #fff">';
          html += '<h1>' + username + '</h1>';
-         html += '<br><b>Followers: </b>' + followers;
-         html += '<br><b>Total Views: </b>' + totalViews;
+         html += '<br><b>Followers: </b><p>' + followers + "</p>";
+         html += '<br><b>Experience: </b><p>' + experience + "</p>";
+         html += '<br><b>Sparks: </b><p>' + sparks + "</p>";
+         html += '<br><b>Channel ID: </b><p>' + channelID + "</p>";
+         html += '<br><b>Total Views: </b><p>' + totalViews + "</p>";
          html += "<br>";
-         html += '<br><b>Current Status:</b>' + online;
-         html += '<br><b>Has a Sub Button:</b>' + sub;
-         html += '<br><b>Has mature content:</b>' + mature;
-         html+= '<br><b>Last Played: </b>' + lastPlayed;
-         html += '<br><b>Joined on: </b>' + joined.replace('T', ' at ');
+         if(online){
+             html += '<br><b><font color="green"><a href="https://beam.pro/' + username + '">Online</a></font></b>';
+         }else{
+             html += '<br><b><font color="red">Offline</font></b>';
+         }
+         html+= '<br><b>Last Played: </b><p>' + lastPlayed + "</p>";
+         html += '<br><b>Joined on: </b><p>' + joined.replace('T', ' at ') + "</p>";
          $('.profile').html(html);
       }).fail(function(data){
-<<<<<<< HEAD
-            html = '<h1>A Twitch user with that name does not exist.';
-=======
            var html = '<h1>A user with that name does not exist.';
->>>>>>> e43ae7550b3b99b26343ad3f528fa09f8d03413f
              $('.profile').html(html);
             return;
       });
