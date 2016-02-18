e47ab7de17180d70b6ac33963b601083c1eef15a
Bacon_Space <thestreamstats@gmail.com>
2016-02-17 19:41:21 -0500

    Updated Stuff

--- js/main.js
+++ js/main.js
@@ -20,6 +20,10 @@ function getData(){
          var Sparks = data['user']['sparks'];
          var ChannelID = data['user']['id'];
          var TotalViews = data['viewersTotal'];
+         var te = data['transcodingEnabled'];
+         var ftl = data['ftl'];
+         var hasVod = data['hasVod'];
+         var suspended = data['suspended'];
          var Online = data['online'];
          if(data['type'] != null){
             var lastPlayed = data['type']['name'];
@@ -30,13 +34,17 @@ function getData(){
          var html = '<center><img src="' + Avatar + '"width="100px" height="100px" style="border:3px solid #fff">';
          html += '<h1>' + Username + '</h1>';
          html += '<br><b>Followers: </b>' + Followers;
-         html += '<br><b>Partnered: </b>' + Partnered;
+         html+= '<br><b>Partnered: </b>' + Partnered;
+         html += '<br><b>FTL: </b>' + ftl;
+         html += '<br><b>HasVod: </b>' + hasVod;
+         html += '<br><b>suspended: </b>' + suspended;
          html += '<br><b>interactive: </b>' + Tetris;
          html += '<br><b>Audience: </b>' + Audience;
          html += '<br><b>Experience: </b>' + Experience;
          html += '<br><b>Sparks: </b>' + Sparks;
          html += '<br><b>Channel ID: </b>' + ChannelID;
          html += '<br><b>Total Views: </b>' + TotalViews;
+         html += '<br><b>transcoding: </b>' + te;
          html += "<br>";
          if(Online){
              html += '<br><b><font color="White"><a href="https://beam.pro/' + Username + '">Online</a></font></b>';
