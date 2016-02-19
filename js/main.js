1b6ada5fa2920b3dfb393503ab1f2669c1f3f109
Bacon_Space <thestreamstats@gmail.com>
2016-02-19 13:13:40 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,11 +20,13 @@ function getData(){
          var lvls = data['user']['level'];
          var Sparks = data['user']['sparks'];
          var ChannelID = data['user']['id'];
+         var Tetrisid = data['tetrisGameId'];
+         var Cover = data['coverId'];
+         var thumbnail = data['thumbnailId'];
+         var StreamMute = data['preferences']['channel:player:muteOwn'];
+         var FollowNotify = data['preferences']['channel:notify:follow'];
          var TotalViews = data['viewersTotal'];
          var te = data['transcodingEnabled'];
-         var ftl = data['ftl'];
-         var hasVod = data['hasVod'];
-         var suspended = data['suspended'];
          var Online = data['online'];
          if(data['type'] != null){
             var lastPlayed = data['type']['name'];
@@ -36,17 +38,22 @@ function getData(){
          html += '<h1>' + Username + '</h1>';
          html += '<br><b>Followers: </b>' + Followers;
          html+= '<br><b>Partnered: </b>' + Partnered;
-         html += '<br><b>FTL: </b>' + ftl;
-         html += '<br><b>HasVod: </b>' + hasVod;
-         html += '<br><b>suspended: </b>' + suspended;
-         html += '<br><b>interactive: </b>' + Tetris;
-         html += '<br><b>Audience: </b>' + Audience;
+          html += '<br><b>Total Views: </b>' + TotalViews;
+         html += '<br><b>---Points---</b>'
          html += '<br><b>Levels: </b>' + lvls
          html += '<br><b>Experience: </b>' + Experience;
          html += '<br><b>Sparks: </b>' + Sparks;
+         html += '<br><b>---ID---</b>'
+         html += '<br><b>Tetris Game ID: </b>' + Tetrisid;
          html += '<br><b>Channel ID: </b>' + ChannelID;
-         html += '<br><b>Total Views: </b>' + TotalViews;
+         html += '<br><b>Cover ID: </b>' + Cover;
+         html += '<br><b>Thumbnail ID: </b>' + thumbnail;
+         html += '<br><b>---Preference---</b>'
+         html += '<br><b>interactive: </b>' + Tetris;
+         html += '<br><b>Audience: </b>' + Audience;
+         html += '<br><b>StreamMute: </b>' + StreamMute;
          html += '<br><b>transcoding: </b>' + te;
+         html += '<br><b>------------</b>'
          html += "<br>";
          if(Online){
              html += '<br><b><font color="White"><a href="https://beam.pro/' + Username + '">Online</a></font></b>';
