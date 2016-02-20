40184a76a125f4f5dce9053dcb1c005324f3bcf0
Bacon_Space <thestreamstats@gmail.com>
2016-02-19 20:29:38 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,13 +37,12 @@ function getData(){
          var html = '<center><img src="' + Avatar + '"width="100px" height="100px" style="border:3px solid #fff">';
          html += '<h1><span class="label label-success">' + Username + '</h1>';
          html += '<br><b><span class="label label-primary">Followers: ' + Followers+ '</b>';
-         html+= '<br><b>Partnered: </b>' + Partnered;
          html += '<br><b><span class="label label-primary">Partnered: ' + Partnered +'</b>';
          html += '<br><b><span class="label label-primary">Total Views: ' + TotalViews +'</b>';
          html += '<br><b><span class="label label-danger">---Points---</b>'
          html += '<br><b><span class="label label-warning">Level: ' + lvls +'</b>';
-         html += '<br><b>Experience: </b>' + Experience;
-         html += '<br><b>Sparks: </b>' + Sparks;
+         html += '<br><b><span class="label label-warning">Experience: ' + Experience +'</b>';
+         html += '<br><b><span class="label label-warning">Sparks: ' + Sparks +'</b>';
          html += '<br><b><span class="label label-primary">---ID---</b>'
          html += '<br><b>Tetris Game ID: </b>' + Tetrisid;
          html += '<br><b>Channel ID: </b>' + ChannelID;
