c530fad45aeee23262295c1e63c271064836a086
Bacon_Space <thestreamstats@gmail.com>
2016-02-19 20:26:04 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,10 +36,11 @@ function getData(){
          var joined = data['createdAt'];
          var html = '<center><img src="' + Avatar + '"width="100px" height="100px" style="border:3px solid #fff">';
          html += '<h1><span class="label label-success">' + Username + '</h1>';
-         html += '<br><b><span class="label label-danger">Followers: ' + Followers+ '</b>';
+         html += '<br><b><span class="label label-primary">Followers: ' + Followers+ '</b>';
          html+= '<br><b>Partnered: </b>' + Partnered;
-         html += '<br><b><span class="label label-danger">Total Views: ' + TotalViews +'</b>';
-         html += '<br><b><span class="label label-primary">---Points---</b>'
+         html += '<br><b><span class="label label-primary">Partnered: ' + Partnered +'</b>';
+         html += '<br><b><span class="label label-primary">Total Views: ' + TotalViews +'</b>';
+         html += '<br><b><span class="label label-danger">---Points---</b>'
          html += '<br><b><span class="label label-warning">Level: ' + lvls +'</b>';
          html += '<br><b>Experience: </b>' + Experience;
          html += '<br><b>Sparks: </b>' + Sparks;
@@ -60,8 +61,8 @@ function getData(){
          }else{
              html += '<br><b><font color="red">Offline</font></b>';
          }
-         html+= '<br><b><span class="label label-danger">Last Played:'+lastPlayed+'</b>';
-         html += '<br><b><span class="label label-danger"style="color: yellow;">Joined on: '+joined+' </b>'.replace('T', ' at ');
+         html+= '<br><b><span class="label label-primary">Last Played:'+lastPlayed+'</b>';
+         html += '<br><b><span class="label label-primary>Joined on: '+joined+' </b>'.replace('T', ' at ');
          $('.profile').html(html);
       }).fail(function(data){
             html = '<p><span class="label label-warning"style="color: black;">A Beam user with that name does not exist.';
