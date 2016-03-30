2c8b45b35a14fb299f658b1cb38d2dead4e91e07
Bacon_Space <thestreamstats@gmail.com>
2016-03-30 12:25:51 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,9 +37,9 @@ function getData(){
          }
          var joined = data['createdAt'];
          
-         var html = '<center><img src="' + Avatar + '"class="img-circle"width="200px" height="200px" style="border:4px solid black">';
+         var html = '<center><img src="' + Avatar + '"class="img-circle"width="100px" height="100px" style="border:4px solid black">';
          
-         html += '<h1><span class="label label-success">' + Username + '</h1>';
+         html += '<h1><center><span class="label label-success">' + Username + '</h1>';
          html += '<br><b><span class="label label-primary">----------</b>'
           html += '<br><b><span class="label label-primary"><font size="3" color="white">Url Link: <a href="https://beam.pro/' + Username + '">Beam.pro/'+ Username +'</a></b>';
          html += '<br><b><span class="label label-primary">Followers: ' + Followers+ '</b>';
